33bd2337a9aa50cd49c601a57bc7cc11b4712e9b
MarinMalouin <p5oc@free.fr>
2019-10-02 13:47:04 +0200

    Fix delete button on edit page
    And add Unique Trick feat

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -6,9 +6,12 @@ use App\Repository\ImageRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\TrickRepository")
+ * @UniqueEntity("title")
  */
 class Trick
 {
